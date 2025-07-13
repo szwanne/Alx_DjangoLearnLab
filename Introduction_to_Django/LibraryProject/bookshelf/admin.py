30d4a987f8a5c097f1e9eab3b81fac9c61ec73fb
szwanne <103660814+szwanne@users.noreply.github.com>
2025-07-13 21:43:26 +0200

    Added the superuser

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # show columns
+    list_filter = ('publication_year', 'author')            # filter sidebar
+    search_fields = ('title', 'author')                     # search bar
