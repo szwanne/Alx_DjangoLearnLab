0fac49850fcaaa422644ad8d5352a05a13d96339
szwanne <103660814+szwanne@users.noreply.github.com>
2025-07-27 22:50:06 +0300

    Simpled form not modellinked was created

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,9 @@
+# bookshelf/forms.py
+
 from django import forms
-from .models import Book
 
 
-class BookSearchForm(forms.Form):
-    q = forms.CharField(label='Search', max_length=100)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(widget=forms.Textarea, label='Your Message')
