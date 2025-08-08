1d77cc44fb08f8a7b62071f03728be7c361443d3
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-08 14:19:46 +0300

    Updated the views file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
