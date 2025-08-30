20e27b1f69438a29fd829ec72b8b233bcc403ca4
szwanne <103660814+szwanne@users.noreply.github.com>
2025-08-30 17:41:53 +0200

    Added the posts django app

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title',
+                  'content', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'post_title', 'author',
+                  'content', 'created_at', 'updated_at']
